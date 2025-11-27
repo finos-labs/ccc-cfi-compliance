1e6e85cb8bf8c15c71f89f3243c08a4e50c0ead7
Rob Moffat <rob@kite9.com>
2025-11-27 09:33:52 +0000

    Trying to get azure install working

--- remote/azure/storageaccount/provider.tf
+++ remote/azure/storageaccount/provider.tf
@@ -2,4 +2,7 @@
 # Uses ARM_SUBSCRIPTION_ID environment variable set by GitHub Actions
 provider "azurerm" {
   features {}
+  
+  # Use Azure AD authentication for storage operations instead of access keys
+  storage_use_azuread = true
 }
